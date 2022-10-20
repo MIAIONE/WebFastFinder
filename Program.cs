b44a3d5ab558d250dc91b3b69c2f2b201ba3ed47
MIAIONE <MIAIONE@Yeah.net>
2022-10-20 11:36:01 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace WebFastFinder
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Stable Diffusion Gradio Fast Finder");
+            Console.WriteLine("The program will not save the URL until you have searched for the range you set, so please do not quit halfway");
+            Console.WriteLine("程序在搜索完您设定的区间后才会保存url,请勿中途退出");
+            Console.WriteLine();
+            Console.Write("Start 起始 (Press Enter default 按回车默认 10000): ");
+            var inputStart = Console.ReadLine();
+            Console.Write("End 结束 (Press Enter default 按回车默认 12000): ");
+            var inputEnd = Console.ReadLine();
+            Console.WriteLine();
+            try
+            {
+                //ArgumentNullException.ThrowIfNull(inputStart);
+                //ArgumentNullException.ThrowIfNull(inputEnd);
+                if((inputStart is null)||(inputStart == ""))
+                {
+                    inputStart = "10000";
+                }
+                if((inputEnd is null)||(inputEnd == ""))
+                {
+                    inputEnd = "12000";
+                }
+                var lists = WebsiteFinder.Search(int.Parse(inputStart), int.Parse(inputEnd));
+                File.WriteAllLines("web_urls.txt", lists);
+                Console.WriteLine();
+                Console.WriteLine("OK - all task complete...");
+            }
+            catch
+            {
+                Console.WriteLine("INVALID PARAMETER 参数非法, 应输入 parameter → int 数字");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WebsiteFinder.cs
@@ -0,0 +1,99 @@
+﻿namespace WebFastFinder
+{
+    internal static class WebsiteFinder
+    {
+        public const string GradioUrl = "https://{0}.gradio.app/";
+        public static readonly List<string> ValidString = new() 
+        {
+            "Stable Diffusion",
+            "txt2img",
+            "img2img"
+        };
+
+        public static readonly List<string> InvalidString = new()
+        {
+            "Bad Gateway",
+            "Internal Server Error",
+            "No interface is running right now",
+            "403 Forbidden",
+            "\"auth_required\": true",
+            "HTTP Error/Application from httpclient [FINDER]"
+        };
+
+        public static readonly ParallelOptions ParallelOptions = new()
+        {
+            MaxDegreeOfParallelism = int.MaxValue //太大会被ban ip
+        };
+
+        public static readonly HttpClientHandler HttpClientHandler = new()
+        {
+            AllowAutoRedirect = true,
+            ServerCertificateCustomValidationCallback = delegate { return true; },
+            MaxConnectionsPerServer = int.MaxValue,
+            ClientCertificateOptions = ClientCertificateOption.Automatic,
+            MaxAutomaticRedirections = int.MaxValue
+        };
+
+        public static List<string> Search(int startindex = 10000, int endindex = 20000)
+        {
+            var validUrlList = new List<string>();
+            try
+            {
+                Parallel.For(startindex, endindex, ParallelOptions, x =>
+                {
+                    var combineUrl = string.Format(GradioUrl, x);
+                    var result = GetHttpResponse(combineUrl).GetAwaiter().GetResult();
+                    foreach (var invalidstr in InvalidString)
+                    {
+                        if (!result.IsVaild(invalidstr))
+                        {
+                            return;
+                        }
+                    }
+                    foreach (var Validstr in ValidString)
+                    {
+                        if (result.IsVaild(Validstr))
+                        {
+                            return;
+                        }
+                    }
+                    validUrlList.Add(combineUrl);
+                    Console.WriteLine(combineUrl);
+                });
+                return validUrlList;
+            }
+            catch 
+            {
+                Console.WriteLine("Program has one or more error , please restart application.");
+                return validUrlList;
+            }
+        }
+
+        public static bool IsVaild(this string context, string str)
+        {
+            if (context.Contains(str, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static async Task<string> GetHttpResponse(string url)
+        {
+            try
+            {
+                var httpclient = new HttpClient(HttpClientHandler, false);
+                var result = await httpclient.GetStringAsync(url);
+                httpclient.Dispose();
+                return result;
+            }
+            catch
+            {
+                return "HTTP Error/Application from httpclient [FINDER]";
+            }
+        }
+    }
+}
